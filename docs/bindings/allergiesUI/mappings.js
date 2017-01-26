4d1c1e6bc17e5d0db859bb7ad7d6c36d16cc4c3e
Ian McNicoll <ian@freshehr.com>
2017-01-26 15:51:54 +0000

    Added experimental bidings folder

---
+++ docs/bindings/allergiesUI/mappings.js
@@ -0,0 +1,5 @@
+{
+  templateID: "IDCR - Adverse Reaction List.v1",
+  queryfields: [cause, causeCode, causeTerminology, terminologyCode,reaction, author,dateCreated],
+  postfields: [cause, causeCode, causeTerminology, reactionCode,reaction, reactionTerminology, author, notes, dateTimeRecorded]
+};
